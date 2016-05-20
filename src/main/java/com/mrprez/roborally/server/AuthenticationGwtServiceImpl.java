f63d6139014032a6a7b40284c056d96eee1c17a0
Guillaume Gagneret <guillaume.gagneret@woonoz.com>
2016-05-20 18:09:42 +0200

    Spring + GWT

--- src/main/java/com/mrprez/roborally/server/AuthenticationGwtServiceImpl.java
+++ src/main/java/com/mrprez/roborally/server/AuthenticationGwtServiceImpl.java
@@ -1,16 +1,11 @@
 package com.mrprez.roborally.server;
 
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.ContextLoader;
-
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.mrprez.roborally.bs.UserService;
 import com.mrprez.roborally.client.AuthenticationGwtService;
 import com.mrprez.roborally.model.User;
 import com.mrprez.roborally.shared.UserGwt;
 
-public class AuthenticationGwtServiceImpl extends RemoteServiceServlet implements AuthenticationGwtService {
+public class AuthenticationGwtServiceImpl extends AbstractGwtService implements AuthenticationGwtService {
 	private static final long serialVersionUID = 1L;
 	
 	private UserService userService;
@@ -18,9 +13,6 @@ public class AuthenticationGwtServiceImpl extends RemoteServiceServlet implement
 	
 	@Override
 	public UserGwt authenticate(String username, String password) throws Exception {
-		ApplicationContext applicationContext = ContextLoader.getCurrentWebApplicationContext();
-		applicationContext.getAutowireCapableBeanFactory()
-				.autowireBeanProperties(this, AutowireCapableBeanFactory.AUTOWIRE_BY_TYPE, false);
 		User user = userService.authenticate(username, password);
 		if(user==null){
 			throw new Exception("Authentification échoué - Login ou mot de passe incorrect");
