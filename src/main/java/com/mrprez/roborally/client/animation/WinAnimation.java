446cab91381406ab16f91e1af637b93be2642c84
Guillaume Gagneret <guillaume.gagneret@woonoz.com>
2016-08-01 13:47:23 +0200

    Improve WinAnimation

--- src/main/java/com/mrprez/roborally/client/animation/WinAnimation.java
+++ src/main/java/com/mrprez/roborally/client/animation/WinAnimation.java
@@ -1,12 +1,15 @@
 package com.mrprez.roborally.client.animation;
 
+import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.canvas.dom.client.Context2d;
+import com.google.gwt.dom.client.ImageElement;
 import com.google.gwt.dom.client.Style.Display;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.Image;
 
 public class WinAnimation extends MoveAnimation {
-	private ScrollPanel scrollPanel;
-	private Label label;
+	private Canvas rocketCanvas = Canvas.createIfSupported();
+	private static Image image = new Image("img/WinnerFlag.png");
+	private static ImageElement imageEl = ImageElement.as(image.getElement());
 	
 	
 	public WinAnimation(){
@@ -15,34 +18,45 @@ public class WinAnimation extends MoveAnimation {
 	
 	@Override
 	public void onStart(){
-		scrollPanel = (ScrollPanel) boardPanel.getParent();
-		label = new Label("A Robot has reached the last Target! AMAZING!");
-		label.addStyleName("winMessage");
-		boardPanel.setWidgetPosition(robotCanvas, 
-				scrollPanel.getOffsetWidth()/2-robotCanvas.getOffsetWidth()/2+scrollPanel.getHorizontalScrollPosition(),
-				scrollPanel.getOffsetHeight()/2-robotCanvas.getOffsetHeight()/2+scrollPanel.getVerticalScrollPosition()+30);
-		boardPanel.add(label,
-				scrollPanel.getOffsetWidth()/2-label.getOffsetWidth()/2+scrollPanel.getHorizontalScrollPosition(),
-				scrollPanel.getOffsetHeight()/2-label.getOffsetHeight()/2+scrollPanel.getVerticalScrollPosition()-30);
-		label.addStyleName("redWinMessage");
-		boardPanel.setWidgetPosition(label,
-				scrollPanel.getOffsetWidth()/2-label.getOffsetWidth()/2+scrollPanel.getHorizontalScrollPosition(),
-				scrollPanel.getOffsetHeight()/2-label.getOffsetHeight()/2+scrollPanel.getVerticalScrollPosition()-30);
+		boardPanel.add(image, boardPanel.getWidgetLeft(robotCanvas), boardPanel.getWidgetTop(robotCanvas));
+		imageEl.getStyle().setDisplay(Display.BLOCK);
+		
+		rocketCanvas.setCoordinateSpaceHeight(97*3);
+		rocketCanvas.setCoordinateSpaceWidth(97*3);
+		boardPanel.add(rocketCanvas, boardPanel.getWidgetLeft(robotCanvas)-97, boardPanel.getWidgetTop(robotCanvas)-97);
 	}
 
 	@Override
 	public void update(double progress) {
-		int intProgress = (int) (progress*10);
-		if(intProgress%2==0){
-			label.removeStyleName("redWinMessage");
-			label.addStyleName("yellowWinMessage");
-			robotCanvas.removeStyleName("redBackGround");
-			robotCanvas.addStyleName("yellowBackGround");
+		Context2d context2d = rocketCanvas.getContext2d();
+		context2d.clearRect(0, 0, 97*3, 97*3);
+		if(progress<0.3){
+			context2d.fillRect(97*(1.5-progress/0.3*0.5), 97*(3-progress/0.3*2), 2, 8);
+			context2d.fillRect(97*(1.5+progress/0.3*0.5), 97*(3-progress/0.3*2), 2, 8);
 		}else{
-			label.removeStyleName("yellowWinMessage");
-			label.addStyleName("redWinMessage");
-			robotCanvas.removeStyleName("yellowBackGround");
-			robotCanvas.addStyleName("redBackGround");
+			for(int i=0; i<20; i++){
+				double r1 = 97*(progress-0.3)/0.7;
+				double r2 = r1/2;
+				
+				context2d.setFillStyle("red");
+				context2d.beginPath();
+				context2d.arc(97+r1*Math.cos(2*Math.PI/20*i), 97+r1*Math.sin(2*Math.PI/20*i), 2, 0, 2*Math.PI);
+				context2d.arc(97+r2*Math.cos(2*Math.PI/20*i), 97+r2*Math.sin(2*Math.PI/20*i), 2, 0, 2*Math.PI);
+				context2d.fill();
+				
+				context2d.setFillStyle("yellow");
+				context2d.beginPath();
+				context2d.arc(2*97+r1*Math.cos(2*Math.PI/20*i), 97+r1*Math.sin(2*Math.PI/20*i), 2, 0, 2*Math.PI);
+				context2d.arc(2*97+r2*Math.cos(2*Math.PI/20*i), 97+r2*Math.sin(2*Math.PI/20*i), 2, 0, 2*Math.PI);
+				context2d.fill();
+			}
+		}
+		
+		int intProgress = (int)(progress*6);
+		if(intProgress % 2 == 0){
+			imageEl.getStyle().setDisplay(Display.BLOCK);
+		}else{
+			imageEl.getStyle().setDisplay(Display.NONE);
 		}
 	}
 	
@@ -54,9 +68,8 @@ public class WinAnimation extends MoveAnimation {
 	@Override
 	public void onComplete(){
 		robotCanvas.getCanvasElement().getStyle().setDisplay(Display.NONE);
-		robotCanvas.removeStyleName("redBackGround");
-		robotCanvas.removeStyleName("yellowBackGround");
-		label.removeFromParent();
+		rocketCanvas.removeFromParent();
+		imageEl.getStyle().setDisplay(Display.NONE);
 	}
 	
 }
