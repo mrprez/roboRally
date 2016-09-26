a1d50281e146ba76580714093888c5319b5992ac
MrPrez <guillaume.gagneret@gmail.com>
2016-09-26 23:07:09 +0200

    Tests

--- src/test/java/com/mrprez/roborally/builder/RobotBuilder.java
+++ src/test/java/com/mrprez/roborally/builder/RobotBuilder.java
@@ -1,16 +1,20 @@
 package com.mrprez.roborally.builder;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.mrprez.roborally.model.Card;
+import com.mrprez.roborally.model.CardStock;
+import com.mrprez.roborally.model.Game;
+import com.mrprez.roborally.model.Game.GameState;
 import com.mrprez.roborally.model.Robot;
-import com.mrprez.roborally.model.board.GameBoard;
 
 public class RobotBuilder {
 	private String username;
 	private boolean ghost = false;
 	private int x = 0;
 	private int y = 0;
-	private List<E>
+	private List<CardPicker> cardPickerList = new ArrayList<RobotBuilder.CardPicker>();
 	
 	
 	private RobotBuilder(){
@@ -32,7 +36,8 @@ public class RobotBuilder {
 	}
 	
 	public RobotBuilder withTranslationCard(int translation){
-		
+		cardPickerList.add(CardPicker.newTranslationCardPicker(translation));
+		return this;
 	}
 	
 	public RobotBuilder withPosition(int x, int y){
@@ -41,16 +46,60 @@ public class RobotBuilder {
 		return this;
 	}
 	
-	Robot buildOnGameBoard(GameBoard gameBoard){
-		Robot robot = new Robot(gameBoard.getSquare(x, y), ghost);
+	Robot buildOnGame(Game game){
+		Robot robot = new Robot(game.getBoard().getSquare(x, y), ghost);
 		robot.setUsername(username);
+		if(game.getState()==GameState.ONGOING){
+			for(CardPicker cardPicker : cardPickerList){
+				robot.getCards().add(cardPicker.pickCard(game.getCardStock()));
+			}
+			while(robot.getCards().size()<9){
+				robot.getCards().add(game.getCardStock().takeCard());
+			}
+		}
 		return robot;
 	}
 	
 	public static class CardPicker{
 		private Integer translation;
 		private Integer rotation;
-		private Integer rotation;
+		private Integer rapidity;
+		
+		private CardPicker(Integer translation, Integer rotation, Integer rapidity) {
+			super();
+			this.translation = translation;
+			this.rotation = rotation;
+			this.rapidity = rapidity;
+		}
+		
+		public static CardPicker newTranslationCardPicker(Integer translation){
+			return new CardPicker(translation, null, null);
+		}
+		
+		public static CardPicker newRotationCardPicker(Integer rotation){
+			return new CardPicker(null, rotation, null);
+		}
+		
+		public static CardPicker newRapidityCardPicker(Integer rapidity){
+			return new CardPicker(null, null, rapidity);
+		}
+		
+		public Card pickCard(CardStock cardStock){
+			for(int i=0; i<CardStock.CARD_NUMBER*2; i++){
+				Card card = cardStock.takeCard();
+				if(translation!=null && translation==card.getTranslation()){
+					return card;
+				}
+				if(rotation!=null && rotation==card.getRotation()){
+					return card;
+				}
+				if(rapidity!=null && rapidity==card.getRapidity()){
+					return card;
+				}
+			}
+			return null;
+		}
+		
 	}
 
 }
